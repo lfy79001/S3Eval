d6f99391f13eba23a2b080d3f2c238b40b8f2bbe
Fangyu Lei <55661995+lfy79001@users.noreply.github.com>
2023-08-15 19:49:43 +0800

    Delete TableQAEval directory

--- TableQAEval/Baselines/1.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-inst")
-model = AutoModelForCausalLM.from_pretrained("Salesforce/xgen-7b-8k-inst")
--- TableQAEval/Evaluation/read_result.py
+++
@@ -1,5 +0,0 @@
-from emf1 import read_results
-
-
-data = read_results('../Results/turbo16k.txt')
-print(len(data))
